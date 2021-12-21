cf67e551c2447f2e9923235174b92987f7eb0876
me-first <vs1010582@gmail.com>
2021-12-21 18:44:06 +0530

    initial commit

--- question1.js
+++ question1.js
@@ -12,33 +12,3 @@ function compare(obj1, obj2) {
 
 const result = compare(obj1, obj2);
 console.log(result);
-
-//Question 2
-// inputString = ​'col1,col2\na,b\nc,d'
-// [{'col1': 'a', 'col2': 'b'}, {'col1': 'c', 'col2': 'd'}]
-// const inputString = "col1,col2\na,b\nc,d";
-
-// const stringArr = inputString.split("\n");
-// const newStringArr = stringArr.map((value) => value.split(","));
-
-const inputString1 = [{ col1: "a", col2: "b" }];
-// 'Col1,col2\na,b';
-
-// const arrayInputString1 = inputString1
-//   .map((obj) => Object.entries(obj))
-//   .flat(2);
-// const arrayOfKeys = [];
-// const arrayOfValues = [];
-// for (let i = 0; i < arrayInputString1.length; i++) {
-//   if (i % 2 === 0) arrayOfKeys.push(arrayInputString1[i]);
-//   else arrayOfValues.push(arrayInputString1[i]);
-// }
-// // console.log(arrayOfKeys);
-// const uniqueArrOfKeysSet = new Set(arrayOfKeys);
-// const uniqueArrOfKeysArrays = [...uniqueArrOfKeysSet];
-// console.log(uniqueArrOfKeysArrays);
-// console.log(arrayOfValues);
-// const stringOfKeys = uniqueArrOfKeysArrays.join(",");
-// const stringOfValues = arrayOfValues.join(',');
-
-// const stringOfObj = stringOfKeys
